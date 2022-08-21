f056f56381cdaf9c17c3fab36fa720009d9fb84f
stancld <daniel.stancl@gmail.com>
2022-08-22 00:02:01 +0200

    wip

--- build_manylinux_wheels.sh
+++ build_manylinux_wheels.sh
@@ -8,4 +8,6 @@ for PYBIN in /opt/python/{cp38-cp38,cp39-cp39,cp310-cp310}/bin; do
     export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
 
     "${PYBIN}/pip" install maturin
+
+    ${PYTHON_SYS_EXECUTABLE} -m maturin build --release
 done
