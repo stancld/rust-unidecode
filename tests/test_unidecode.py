25b07931077abea74894bdcfb6e20d33a8226762
Daniel Stancl <46073029+stancld@users.noreply.github.com>
2022-08-21 21:59:57 +0200

    Test python unidecode package equivalence (#2)

--- tests/test_unidecode.py
+++ tests/test_unidecode.py
@@ -1,4 +1,12 @@
 from fast_unidecode import unidecode
+from unidecode import unidecode as py_unidecode
+
+
+def test_py_unidecode_equivalence():
+    valid_unicode = tuple(range(0x0, 0xD7FF + 1)) + tuple(range(0xE000, 0x10FFFF + 1))
+    for _u in valid_unicode:
+        u = hex(_u)
+        assert unidecode(u) == py_unidecode(u)
 
 
 def test_conversion():
