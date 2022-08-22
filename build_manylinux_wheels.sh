5fc07d95a909d72f101c09a8a6f35cb4271e1db1
Daniel <daniel.stancl@rossum.ai>
2022-08-22 13:59:43 +0200

    Tiny fixes :cool:

--- build_manylinux_wheels.sh
+++ build_manylinux_wheels.sh
@@ -6,10 +6,8 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 for PYBIN in /opt/python/{cp38-cp38,cp39-cp39,cp310-cp310}/bin; do
     export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
-    export python3=${PYTHON_SYS_EXECUTABLE}
-    alias python3=${PYTHON_SYS_EXECUTABLE}
 
-    "${PYBIN}/pip" install maturin
+    ${PYTHON_SYS_EXECUTABLE} -m pip install maturin
 
-    ${PYTHON_SYS_EXECUTABLE} -m maturin build --release
+    ${PYTHON_SYS_EXECUTABLE} -m maturin build --release --interpreter ${PYTHON_SYS_EXECUTABLE}
 done
